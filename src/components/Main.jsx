a99d211409f58594c201efc95049d15ecee10c27
Melozzo <carmecamma1995@gmail.com>
2024-03-20 17:47:20 +0100

    [create]: structure to board component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,15 @@
 import './Main.css'
+import React, { createElement } from 'react';
 
 function Main() {
   const userName = 'FRANCOLDO'
 
   return (
     <main>
-      <h1>{userName} welcome to melo-react</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis labore ducimus tempora alias possimus beatae esse ullam doloribus. Incidunt, eveniet error voluptatum placeat optio eius nisi culpa ipsam, expedita animi eligendi amet sunt suscipit consequuntur molestiae labore excepturi natus et. Consequatur nulla tempore aliquam aspernatur aut fuga odit ipsa sapiente numquam recusandae est provident dolore architecto neque illum maxime possimus eaque doloremque explicabo, hic cumque dolores error exercitationem quisquam. Tempora tempore fuga quisquam quidem similique id, a porro accusamus unde, ea quaerat. Cumque, iste? Non provident iure hic est aspernatur quas iusto quia! Quisquam iste minima, voluptatum deserunt perferendis accusantium numquam culpa pariatur eos tempora quos nisi nihil, impedit enim illum animi quod natus architecto et libero distinctio voluptates neque consequatur. Aperiam veritatis impedit quam omnis? Eveniet illum animi repellendus fugiat tenetur recusandae similique pariatur molestiae explicabo in voluptate reiciendis commodi, inventore cum nemo dolore ipsum natus hic labore distinctio minima alias nisi dicta quisquam. Aut qui deserunt dolores quaerat iste numquam dicta exercitationem, quasi aliquid dolorem, placeat, ducimus provident doloremque quisquam maiores fugit earum ea eos. Ipsa consequatur nesciunt fugiat porro, minus perspiciatis iure ab, possimus minima labore distinctio excepturi itaque nemo voluptatum culpa explicabo sed recusandae magni, quam ullam optio? Illum cupiditate ab quis incidunt tempora quidem sunt nesciunt maiores, doloribus deserunt nulla ad temporibus molestias aspernatur expedita iste magni obcaecati iure quasi accusantium omnis quo ipsum sed dolor! Nam doloribus labore est laudantium perferendis repellat, commodi quia suscipit error deserunt, illo ea rerum quidem beatae harum voluptatibus.</p>
+      <h2>Tris di {userName}</h2>
+      <div className='trisTable'>
+       <Board />
+      </div>
     </main>
   );
 }
